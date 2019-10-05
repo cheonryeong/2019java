6c07017f24496bd2300899089839d2c7970d431c
cheonryeong <dlsnsiti@gmail.com>
2019-10-06 01:14:25 +0900

    baekjoon 11654 source

---
+++ q11654/Ascii.java
@@ -0,0 +1,20 @@
+package q11654;
+
+import java.util.Scanner;
+
+/*
+이름: 이영선
+학번: 201732023
+내용: 11654 - 아스키코드 값 출력
+ */
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String a;
+		a = s.nextLine();
+		System.out.println(a.hashCode());
+	}
+}
